a54c541686ce0f78bb92c2874c74d46af5643826
LuizFFarias <rm99519@fiap.com.br>
2023-05-09 09:50:44 -0300

    js alterado

--- JS/script.js
+++ JS/script.js
@@ -40,11 +40,44 @@
 //     console.log("Itens do Novo Array: " + nr);
 // })
 
-const aElement = document.getElementsByTagName("a");
-const aElementArray = [...aElement];
-console.log(aElementArray);
+//Converter em array
+// const aElement = document.getElementsByTagName("a");
+// const aElementArray = [...aElement];
+// console.log(aElementArray);
 
 //Imprimindo atributos dos elementos recuperados...
-aElementArray.forEach((a)=>{
-    console.log(a.innerHTML)
+// aElementArray.forEach((a)=>{
+//     console.log(a.innerHTML)
+// })
+
+const inputUser = document.querySelector("input[type='email']");
+
+inputUser.addEventListener("focus", ()=>{
+    inputUser.setAttribute("style","outline-color: #ff0000;");
 })
+
+inputUser.addEventListener("keyup", ()=>{
+    const lblUser = document.querySelector("label[for='idEmail']");
+    if(inputUser.value.length < 5){
+        lblUser.innerHTML = "<span style:'color:#ff0000'>Email : ( Mínimo de 5 caractéres )</span>"
+        inputUser.setAttribute("style","outline-color: #ff0000;");
+    }else{
+        lblUser.innerHTML = "<span style: color:'#00ff00'>Email</span>"
+        inputUser.setAttribute("style", "outline-color: #00ff00");
+    }
+
+})
+
+//MOSTRAR SENHA 
+const eyePass = document.querySelector(".fa-eye");
+eyePass.addEventListener("click", ()=>{
+    const inputPass = document.querySelector("#idPass");
+
+    //Alterar o type
+    if(inputPass.getAttribute("type") == "password"){
+        inputPass.setAttribute("type", "text");
+        eyePass.setAttribute("i", "<i class='fa fa-eye-slash' aria-hidden='true'></i>")
+    }else{
+        inputPass.setAttribute("type", "password");
+    }
+});

d4ea1d27b8b28e05cd61bfae74f6c00e7b5fd6a5
LuizFFarias <rm99519@fiap.com.br>
2023-05-16 10:17:23 -0300

    comit

--- JS/login.js
+++ JS/login.js
@@ -1,12 +1,29 @@
-    function inserenumero(botao,visor){
-        const meuBotao = document.getElementById(botao);
-        const meuVisor = document.getElementBy(visor);
+    
+function insereNumero(botao,visor) {
+    if (botao.value == "+" || botao.value == "-" || botao.value == "*" || botao.value == "/") {
+        if (visor.value[visor.value.length-1] == "+" ||visor.value[visor.value.length-1] == "-" || visor.value[visor.value.length-1] == "*" || visor.value[visor.value.length-1] == "/") {
+            visor.value.slice(visor.value.length-1)
+        } else {
+            visor.value += botao.value
+        }
+    }
+    else{
+        visor.value += botao.value
+    }
+    
+    
 
-        meuVisor.value += meuBotao.value;
-    }
-    function resultado(visor){
-        const meuVisor = document.getElementById(visor);
-        let armazenadorDeNumeros = [];
-        armazenadorDeNumeros =[...meuVisor.value];
-        console.log(armazenadorDeNumeros);
-    }
+}
+
+//TRANSFORME A FUNÇÃO EM UMA FUNÇÃO QUE RECEBA TODAS AS OPERAÇÕES
+function resultado(visor) {
+   try {
+       visor.value = eval(visor.value);
+    
+   } catch (error) {
+        console.log(error);
+        if (visor.value[visor.value.length] == "+" || visor.value[visor.value.length] == "-" || visor.value[visor.value.length] == "*" || visor.value[visor.value.length] == "/" ) {
+            visor.value.slice(visor.value.length);
+        }
+   }
+}

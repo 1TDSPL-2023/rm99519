87714786911876c9487aa4691f0e154ab141de90
LuizFFarias <rm99519@fiap.com.br>
2023-05-18 12:24:37 -0300

    js login adicionado

--- JS/login.js
+++ JS/login.js
@@ -1,29 +1,76 @@
-    
-function insereNumero(botao,visor) {
-    if (botao.value == "+" || botao.value == "-" || botao.value == "*" || botao.value == "/") {
-        if (visor.value[visor.value.length-1] == "+" ||visor.value[visor.value.length-1] == "-" || visor.value[visor.value.length-1] == "*" || visor.value[visor.value.length-1] == "/") {
-            visor.value.slice(visor.value.length-1)
+//
+let listaDeUsuario = [
+{
+    nomeCompleto : "Edulado Capacho",
+    emailUsuario : "dudu@email.com",
+    senhaUsuario : "123456"
+},
+{
+    nomeCompleto : "Jose das Couves",
+    emailUsuario : "jose@email.com",
+    senhaUsuario : "123456"
+},
+{
+    nomeCompleto : "Paulo Cabuloso",
+    emailUsuario : "paulo@email.com",
+    senhaUsuario : "123456"
+},
+{
+    nomeCompleto : "Leonardo Tufo",
+    emailUsuario : "lele@email.com",
+    senhaUsuario : "123456"
+},
+{
+    nomeCompleto : "Mathias Aires",
+    emailUsuario : "momo@email.com",
+    senhaUsuario : "123456"
+},
+];
+
+
+
+
+localStorage.setItem("key", Object)
+
+
+const inputEmail = document.querySelector("#idEmail")
+const inputSenha = document.querySelector("#idPass")
+const msgStatus = document.querySelector("#msg")
+//captura de todos os elementos
+addEventListener("click", (evt)=>{
+
+    //criando um objeto
+    let usuarioLogado = {
+        emailUsuarioLogado : inputEmail.value,
+        senhaUsuarioLogado : inputSenha.value
+    }
+    let usuarioValidado = {};
+    //recuperando um elemento do DOM
+    if(evt.target.id == "btnSubmit"){
+        // listaDeUsuario.forEach((usuario)=>{
+
+        //     if (usuarioLogado.emailUsuarioLogado == usuario.emailUsuario && usuarioLogado.senhaUsuarioLogado == usuario.senhaUsuario) {
+        //         console.log("Validado!")
+        //     } else {
+        //         console.log("Não Validado!")
+        //     }
+
+        // });
+        for(let x = 0; x < listaDeUsuarios.length; x++){
+            if(usuarioLogado.emailUsuarioLogado == listaDeUsuario[x].emailUsuario && usuarioLogado.senhaUsuarioLogado == listaDeUsuario[x].senhaUsuario){
+            //passando para o obj oq foi validado
+            usuarioValidado = listaDeUsuarios[x];
+            break;
+            }
+        }
+        if (usuarioLogado.emailUsuarioLogado == usuarioValidado.emailUsuario && usuarioLogado.senhaUsuarioLogado == usuarioValidado.senhaUsuario) {
+            msgStatus.setAttribute("style", "color:green");
+            msgStatus.innerHTML = `<span><strong>O usuário ${usuarioValidado.nomeCompleto} logou com sucesso!</strong></span>`;
         } else {
-            visor.value += botao.value
+            msgStatus.setAttribute("style","color:red");
+            msgStatus.innerHTML = "<span><strong>Nome de usuário ou senha inválidos...</strong></span>";
+            
         }
     }
-    else{
-        visor.value += botao.value
-    }
     
-    
-
-}
-
-//TRANSFORME A FUNÇÃO EM UMA FUNÇÃO QUE RECEBA TODAS AS OPERAÇÕES
-function resultado(visor) {
-   try {
-       visor.value = eval(visor.value);
-    
-   } catch (error) {
-        console.log(error);
-        if (visor.value[visor.value.length] == "+" || visor.value[visor.value.length] == "-" || visor.value[visor.value.length] == "*" || visor.value[visor.value.length] == "/" ) {
-            visor.value.slice(visor.value.length);
-        }
-   }
-}
+});
